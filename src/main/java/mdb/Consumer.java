722e1ef0d526e18ab53501dd10c2b7589e3a09e4
GianluDeveloper <github@gianlu.dev>
2021-02-18 15:22:50 +0100

    ✉ Add MDB

---
+++ src/main/java/mdb/Consumer.java
@@ -0,0 +1,40 @@
+package mdb;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * Message-Driven Bean implementation class for: Consumer
+ */
+@MessageDriven(activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "java:/jms/queue/CODA") })
+public class Consumer implements MessageListener {
+
+	/**
+	 * Default constructor.
+	 */
+	public Consumer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see MessageListener#onMessage(Message)
+	 */
+	public void onMessage(Message message) {
+		// TODO Auto-generated method stub
+		TextMessage textMessage = (TextMessage) message;
+		try {
+			System.out.println("Ho ricevuto -> " + textMessage.getText());
+
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+		System.out.println(message);
+	}
+
+}
